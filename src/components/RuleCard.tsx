384bcbc69eba642c9b6f68366cbd4d5e4b8d8a67
Chris Wilson <divclass01@gmail.com>
2026-07-12 22:26:24 -0400

    Pass workspace controls into rule cards

--- src/components/RuleCard.tsx
+++ src/components/RuleCard.tsx
@@ -2,19 +2,25 @@ import type { RuleCard as RuleCardType, RuleRollHistoryEntry } from "../types/ru
 import { useRuleCardState } from "../hooks/useRuleCardState";
 import { RuleCardBack } from "./RuleCardBack";
 import { RuleCardFront } from "./RuleCardFront";
+import type { WorkspaceCardControls } from "./RuleCardWorkspaceActions";
 
 type RuleCardProps = {
   card: RuleCardType;
   onRoll: (entry: RuleRollHistoryEntry) => void;
+  workspaceControls?: WorkspaceCardControls;
 };
 
-export const RuleCard = ({ card, onRoll }: RuleCardProps) => {
+export const RuleCard = ({ card, onRoll, workspaceControls }: RuleCardProps) => {
   const controller = useRuleCardState({ card, onRoll });
 
   return (
     <article className={`rule-card ${controller.isFlipped ? "is-flipped" : ""}`}>
       <div className="rule-card__inner">
-        <RuleCardFront card={card} controller={controller} />
+        <RuleCardFront
+          card={card}
+          controller={controller}
+          workspaceControls={workspaceControls}
+        />
         <RuleCardBack card={card} controller={controller} />
       </div>
     </article>
