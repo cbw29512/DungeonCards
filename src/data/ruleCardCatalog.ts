3f2007cbc080c6da5c62918120d12a50d63645e4
Chris Wilson <divclass01@gmail.com>
2026-07-12 21:19:18 -0400

    Add expanded spell catalogs

--- src/data/ruleCardCatalog.ts
+++ src/data/ruleCardCatalog.ts
@@ -1,12 +1,16 @@
 import type { RuleCard } from "../types/ruleCards";
 import { magicItemRuleCards } from "./magicItemRuleCards";
+import { spellAreaRuleCards } from "./spellAreaRuleCards";
+import { spellFocusedRuleCards } from "./spellFocusedRuleCards";
 import { spellRuleCards } from "./spellRuleCards";
 import { trapRuleCards } from "./trapRuleCards";
 import { weaponRuleCards } from "./weaponRuleCards";
 
 export const playerRuleCards: RuleCard[] = [
   ...weaponRuleCards,
-  ...spellRuleCards
+  ...spellRuleCards,
+  ...spellAreaRuleCards,
+  ...spellFocusedRuleCards
 ];
 
 export const dmRuleCards: RuleCard[] = [
