957b60dc7b0aa79152aa45991ac60a5720eb66d0
Chris Wilson <divclass01@gmail.com>
2026-07-12 21:07:42 -0400

    Define weapon catalog records

---
+++ src/data/weaponCatalogTypes.ts
@@ -0,0 +1,10 @@
+export type WeaponDefinition = {
+  id: string;
+  name: string;
+  damage?: string;
+  damageType?: "Bludgeoning" | "Piercing" | "Slashing";
+  properties: string;
+  versatileDamage?: string;
+  mastery?: "Cleave" | "Graze" | "Nick" | "Push" | "Sap" | "Slow" | "Topple" | "Vex";
+  icon?: string;
+};
