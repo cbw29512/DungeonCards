3dbaae69dfc4e91dcd811abc7a03c69a9b9b8517
Chris Wilson <divclass01@gmail.com>
2026-07-15 17:40:53 -0400

    Define official SRD source configuration

---
+++ scripts/srd/source-config.mjs
@@ -0,0 +1,24 @@
+export const srdSources = [
+  {
+    edition: "srd-5.1-2014",
+    version: "5.1",
+    pdfUrl: "https://media.dndbeyond.com/compendium-images/srd/5.1/SRD_CC_v5.1.pdf",
+    spellPages: [114, 203],
+    monsterPages: [254, 403],
+    attribution: "This work includes material from the System Reference Document 5.1 by Wizards of the Coast LLC, available at https://www.dndbeyond.com/srd. The SRD 5.1 is licensed under the Creative Commons Attribution 4.0 International License."
+  },
+  {
+    edition: "srd-5.2.1-2024",
+    version: "5.2.1",
+    pdfUrl: "https://media.dndbeyond.com/compendium-images/srd/5.2/SRD_CC_v5.2.1.pdf",
+    spellPages: [107, 176],
+    monsterPages: [258, 364],
+    attribution: "This work includes material from the System Reference Document 5.2.1 (SRD 5.2.1) by Wizards of the Coast LLC, available at https://www.dndbeyond.com/srd. The SRD 5.2.1 is licensed under the Creative Commons Attribution 4.0 International License."
+  }
+];
+
+export const generatedPaths = {
+  spells: "src/generated/srd-spells.json",
+  monsters: "src/generated/srd-monsters.json",
+  manifest: "src/generated/srd-manifest.json"
+};
