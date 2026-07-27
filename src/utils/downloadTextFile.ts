3e27974229a83b55fdf5689d621b378ab662622b
Chris Wilson <divclass01@gmail.com>
2026-07-27 12:49:19 -0400

    Add browser text download utility

---
+++ src/utils/downloadTextFile.ts
@@ -0,0 +1,16 @@
+export const downloadTextFile = (
+  filename: string,
+  text: string,
+  mimeType = "application/json"
+): void => {
+  const blob = new Blob([text], { type: `${mimeType};charset=utf-8` });
+  const url = URL.createObjectURL(blob);
+  const anchor = document.createElement("a");
+  anchor.href = url;
+  anchor.download = filename;
+  anchor.hidden = true;
+  document.body.append(anchor);
+  anchor.click();
+  anchor.remove();
+  URL.revokeObjectURL(url);
+};
